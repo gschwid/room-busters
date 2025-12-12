43ccc172aac37c84a0e1d432e504985d472e1963
Grant Schwidder <gschwid@gmail.com>
2025-12-12 05:22:01 +0000

    Created working localization with existing map generated by exploring map.

--- launch/slam_launch.py
+++ launch/slam_launch.py
@@ -2,32 +2,73 @@
 import launch
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
+
+    map_file_path = PathJoinSubstitution(
+        [FindPackageShare('room-busters'), 'maps', 'rtabmap.db']
+    )
+
+    rviz_config = PathJoinSubstitution(
+        [FindPackageShare('room-busters'), 'rviz', 'occupancy_config.rviz']
+    )
+
+
     return launch.LaunchDescription([
         Node(
             package='rtabmap_slam',
             executable='rtabmap',
             name='rtabmap',
             parameters=[
-                # Set parameters here or point to .ini file
                 {"frame_id": "base_link"},
                 {"odom_frame_id": "odom"},
-                #{"database_path": "/workspace"}, # For saving/loading
+                {"database_path": map_file_path}, # Uses already mapped data.
                 {"subscribe_depth": False},
                 {"subscribe_scan": True},
                 {"queue_size": 30},
                 {"approx_sync": True},
                 {"use_sim_time": True}
             ],
-            # Remap topics to your robot's actual topics
             remappings=[
                 ("/rgb/image", "/camera/image_raw"),
                 ("/rgb/camera_info", "/camera/camera_info"),
-                ("/imu", "/DONTLOOK"),
+                ("/imu", "/DONTLOOK"), # It was auto subscribing to the IMU when the proper transformations werent set up.
                 ("/scan", "/scan"),
                 ("/odom", "/odom")
             ],
             output='screen',
         ),
+        Node(
+            package='rtabmap_viz',
+            executable='rtabmap_viz',
+            name='rtabmap_viz',
+            parameters=[
+                {"frame_id": "base_link"},
+                {"odom_frame_id": "odom"},
+                {"subscribe_scan": True},
+                {"subscribe_depth": False},
+                {"subscribe_stereo": False},
+                {"subscribe_rgbd": False},
+                {"queue_size": 10},
+                {"approx_sync": True},
+                {"use_sim_time": True}
+            ],
+            remappings=[
+                ("/rgb/image", "/camera/image_raw"),
+                ("/rgb/camera_info", "/camera/camera_info"),
+                ("/imu", "/DONTLOOK"), # It was auto subscribing to the IMU when the proper transformations werent set up.
+                ("/scan", "/scan"),
+                ("/odom", "/odom")
+            ],
+            output='screen',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz_display',
+            arguments=['-d', rviz_config], 
+            output='screen'
+        )
+
     ])
--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
