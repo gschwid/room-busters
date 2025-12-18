0cd6ce56de53c92472028c19d412e4c5d83dc953
Henry Mahnke <henry.mahnke16@gmail.com>
2025-12-18 10:57:51 -0600

    dumb_vacuum comments

--- room_busters/dumb_vacuum.py
+++ room_busters/dumb_vacuum.py
@@ -24,30 +24,23 @@ class dumb_vacuum(Node):
 
     def scan_callback(self, msg):
         # msg.ranges contains the distance readings
-        # For TurtleBot3, index 0 is usually directly in front
-        # Values are in meters
-        # from the scan object we get angle min = 0 and max = 6.28
-        # we get angle increment 0.017, this is about 1 degree 
-        # so i will scan in the front 20 degrees that is -10 -> 0 
-        # which is index 350-: and then 0->10 is  0:10
         if msg.ranges:
+            # define distances in field of view
             self.distances_in_range = msg.ranges[340:] + msg.ranges[0:20]
             # self.get_logger().info(f'Distance ahead: {self.distances_in_range}')
             move_message = TwistStamped()
             move_message.header.stamp = self.get_clock().now().to_msg()
 
+            # check a minimum distance to see if collision
             if(min(self.distances_in_range) > 0.7):
+                # no collision
                 self.get_logger().info(f'dis working?: {min(self.distances_in_range)}')
 
-                # keep moving forward
+                # move forward
                 move_message.twist.linear.x = 5.0
                 move_message.twist.angular.z = 0.0
             else:
-                # rotate i guess until there is nothing in front of us?
-                # what we could do, is rotate 360 in a "scanning pass" using the imu
-                # then we could pick a random direction in the free space
-                # other potentials are visualizing all of the places that we have been
-                
+                # yes collision, turn until no collision
                 move_message.twist.linear.x = 0.0
                 move_message.twist.angular.z = 1.0
             self.cmd_pub.publish(move_message)
