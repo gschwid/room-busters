da79896ad12fd9d587f078cb126395d0162807e3
Henry Mahnke <henry.mahnke16@gmail.com>
2025-12-18 10:55:19 -0600

    dumb vacuum v2 comments

--- room_busters/dumb_vacuum_v2.py
+++ room_busters/dumb_vacuum_v2.py
@@ -1,3 +1,7 @@
+# Dumb_vacuum_v2 
+# fix issues from dumb_vacuum to avoid hugging walls by adding
+# a probablistic element to the direction of travel
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
@@ -7,14 +11,8 @@ import random
 import math
 import numpy as np
 from enum import Enum
-# The purpose of this node is to build off of dumb_vacuum to be slightly less dumb
-# the proposed way of doing this, is to instead of looking straight forward, and viewing through a small
-# field of view that leads to the robot "sticking" to the edges, what if we used the whole lidar, found the max 
-# in a sliding window, say of 10 degrees, and then move in the center of that direction
-# if there are multiple sliding windows that are optimal, we choose arbitrarily. 
-# this adds a sense of randomness and might become completeness?!? total guess, but hopefully better
 
-# 
+# small state manager
 class state(Enum):
     FindNewDir = 1
     InFindDir = 2
@@ -37,16 +35,17 @@ class dumb_vacuum_v2(Node):
             self.odom_callback,
             10
         )
+
         self.battery = 100
         self.min_dist_lidar = 0.6
         self.angle_setpoint = 0.0
         self.state = state.FindNewDir
         self.recharge_val = 10
 
-        # But when do we set self.vacuuming to false and use path planning?
-        
         self.laser_sub  # prevent unused variable warning
         self.odom_message = Odometry()
+
+    # AI generated function for getting yaw from a quaternion
     def get_yaw(self):
         if self.odom_message is None:
             return 0.0
@@ -54,25 +53,25 @@ class dumb_vacuum_v2(Node):
         siny_cosp = 2 * (q.w * q.z + q.x * q.y)
         cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
         return math.degrees(math.atan2(siny_cosp, cosy_cosp))
+    # most logic happens here in the scan callback
     def scan_callback(self, msg):
-        # msg.ranges contains the distance readings
-        # For TurtleBot3, index 0 is usually directly in front
-        # Values are in meters
-        # from the scan object we get angle min = 0 and max = 6.28
-        # we get angle increment 0.017, this is about 1 degree 
-        # so i will scan in the front 20 degrees that is -10 -> 0 
-        # which is index 350-: and then 0->10 is  0:10
-        # make the battery drain quite fast so that you can see it
+        # while I don't believe it is common practice to have 
+        # logic in a sensor callback function
+        # it is working, and I don't know what is common practice!
+
+        # log battery state
         self.get_logger().info(f'battery: {self.battery}')
 
+        # define battery threshold
         battery_above_threshold = self.battery > self.recharge_val
+
         if msg.ranges and battery_above_threshold:
             self.battery =self.battery-.1
-            # find max in sliding window 
-            # instead of finding the max sliding window we just want to find a valid sliding window 
-            # becuase otherwise we would avoid walls which isn't ideal
+
             move_message = TwistStamped()
             move_message.header.stamp = self.get_clock().now().to_msg()
+
+            # 40 degree field of view in front of robot
             distances_in_range = msg.ranges[340:] + msg.ranges[0:20]
 
             if(min(distances_in_range) < self.min_dist_lidar):
@@ -83,11 +82,13 @@ class dumb_vacuum_v2(Node):
             if self.state == state.FindNewDir:
                 self.get_logger().info(f'Finding New Direction...')
 
+                # ~ 10 degree sliding window
                 sliding_window_len = 10
-                # find a direction (10 ish degree window) 
-                # that has all distances larger than 1
                 valid_starting_indices = []
+                
+                # find the viable sliding windows
                 for i in range(len(msg.ranges)):
+                    # handling the edge effects of the array
                     if i+sliding_window_len <= len(msg.ranges):
                         arr = msg.ranges[i:i+sliding_window_len]
                         is_valid_range = (np.array(arr) > self.min_dist_lidar).all()
@@ -107,12 +108,17 @@ class dumb_vacuum_v2(Node):
                 if not valid_starting_indices:
                     return
 
+                # pick a random index 
                 rand_list_idx = random.randint(0, len(valid_starting_indices)-1)
                 
+                # the scan index is the first of the sliding window
                 chosen_scan_index = valid_starting_indices[rand_list_idx]
                 
                 # then we want to turn to the value of that 
+                # add 5 to get the center of that sliding window
                 angle_ind_to_turn_to = chosen_scan_index + 5
+
+                # find angle
                 angle_rad = msg.angle_increment * angle_ind_to_turn_to + msg.range_min
                 
                 # Convert to degrees before adding to yaw
@@ -122,12 +128,15 @@ class dumb_vacuum_v2(Node):
                 self.angle_setpoint = angle_deg + self.get_yaw()
                 self.state = state.InFindDir
             elif self.state == state.InFindDir:
-
                 self.get_logger().info(f'Moving to new Direction...')
                 pgain = 3.1
                 error = self.angle_setpoint - self.get_yaw()
-                # normalize error
-                # the error for the
+                
+                # normalize error avoid wrap around 
+                # ex. angle setpoint is -180, yaw is 170
+                # error = -350 
+                # normalized error = -350 + 180 = -170 
+                # -170 % 360 - 190
                 error = (error + 180) % 360 - 180
                 error_rad = math.radians(error)
                 self.get_logger().info(f'error: {error}')
@@ -150,9 +159,6 @@ class dumb_vacuum_v2(Node):
 
     def odom_callback(self,msg): 
         self.odom_message = msg
-
-
-    
 
 
 def main(args=None):
